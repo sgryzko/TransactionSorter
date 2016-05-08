4375556eae351f9613fa9acc14cf41867d87c6bf
Shane Gryzko <sgryzko@gmail.com>
2016-05-09 00:31:32 +0100

    Convert to JUCE unit tests

---
+++ UnitTests/Source/Main.cpp
@@ -0,0 +1,21 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic startup code for a Juce application.
+
+  ==============================================================================
+*/
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+
+//==============================================================================
+int main (int argc, char* argv[])
+{
+	UnitTestRunner runner;
+	runner.runAllTests();
+
+    return 0;
+}
